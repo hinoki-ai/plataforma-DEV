f68b8770d089047b16b47ceda81b0e78cd034067
Astral Deploy <agustinaramac@gmail.com>
2025-11-18 19:51:32 -0300

    fix: resolve Convex schema export issue by explicitly constructing schema object

--- convex/schema/index.ts
+++ convex/schema/index.ts
@@ -9,22 +9,40 @@
  */
 
 // Authentication and User Management
+import * as authSchema from "./auth";
 export * from "./auth";
 
 // Institution and Team Management
+import * as institutionSchema from "./institution";
 export * from "./institution";
 
 // Educational Core (Students, Courses, Activities)
+import * as educationalSchema from "./educational";
 export * from "./educational";
 
 // Libro de Clases (Chilean Class Book)
+import * as libroClasesSchema from "./libro_clases";
 export * from "./libro_clases";
 
 // Convivencia Escolar (School Coexistence)
+import * as convivenciaSchema from "./convivencia";
 export * from "./convivencia";
 
 // Planning and Calendar
+import * as planningSchema from "./planning";
 export * from "./planning";
 
 // Media and Communication
+import * as mediaSchema from "./media";
 export * from "./media";
+
+// Export combined schema object for use in defineSchema
+export const schemaObject = {
+  ...authSchema,
+  ...institutionSchema,
+  ...educationalSchema,
+  ...libroClasesSchema,
+  ...convivenciaSchema,
+  ...planningSchema,
+  ...mediaSchema,
+};
